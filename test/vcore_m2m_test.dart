16c6ed6faf8e86f7a26d188e9d472461f0174362
Anders Holmgren <andersmholmgren@gmail.com>
2016-05-07 13:30:16 +1000

    reworking serialiser into parts

--- test/vcore_m2m_test.dart
+++ test/vcore_m2m_test.dart
@@ -5,17 +5,14 @@ import 'package:vcore_m2m/vcore_m2m.dart';
 import 'package:test/test.dart';
 import 'package:vcore_m2m/src/model/transform.dart';
 import 'package:vcore_ecore/vcore_ecore.dart';
+import 'package:vcore_ecore/vcore_ecore_meta.dart' as e;
 import 'package:vcore/vcore.dart';
 
 void main() {
   group('A group of tests', () {
-
     test('First Test', () {
-
-      new ValueClassRelation((ValueClassRelationBuilder b) => b
-      ..from = EClass
-      );
-
+      new ValueClassRelation(
+          (ValueClassRelationBuilder b) => b..from = e.EClass);
     });
   });
 }
