a0e3c86904f713950260c5a092258ea1c129c1ec
Anders Holmgren <andersmholmgren@gmail.com>
2016-05-15 15:49:14 +1000

    relate schema to valueClass

--- lib/src/transformer.dart
+++ lib/src/transformer.dart
@@ -1,5 +1,6 @@
 import 'package:vcore_m2m/src/model/transform.dart';
 import 'package:vcore/vcore.dart';
+import 'package:built_collection/src/list.dart';
 
 typedef Classifier ClassifierMirrorSystem(Type type);
 
@@ -11,6 +12,10 @@ class Transformer {
   Transformer(this.packageRelation, this.sourceMirrorSystem);
 
   /*=T*/ transform/*<F, T>*/(/*=F*/ from) {
+    return _transform(from, new _TransformationContext());
+  }
+
+  /*=T*/ _transform/*<F, T>*/(/*=F*/ from, _TransformationContext context) {
     final fromClassifier = sourceMirrorSystem(from.runtimeType);
 
     /**
@@ -20,6 +25,57 @@ class Transformer {
     final classifierRelation = packageRelation.classifierRelations
         .firstWhere((cr) => cr.from == fromClassifier);
 
+    if (classifierRelation is ValueClassRelation) {
+      final toBuilder = context.builderFor(classifierRelation.to);
+      ValueClassRelation classRelation = classifierRelation;
+      classRelation.propertyRelations.forEach((pr) {
+        _transformProperty(fromClassifier, toBuilder, pr, context);
+      });
+    }
 //    classifierRelation.
   }
+
+  void _transformProperty(
+      ValueClass fromClassifier,
+      ValueClassBuilder toBuilder,
+      PropertyRelation pr,
+      _TransformationContext context) {
+    final Property sourceProperty = _resolvePath(fromClassifier, pr.fromPath);
+    if (sourceProperty == null) {
+      print('No property found for path ${pr.fromPath.join('.')} '
+          'on $fromClassifier');
+      // ignore. Is that correct or an error?
+    } else {
+      final Property targetProperty = _resolveAndCreate(toBuilder, pr.toPath);
+      // can't be null
+      if (targetProperty.isCollection != sourceProperty.isCollection) {
+        String _m(Property p) =>
+            'a value that is ${p.isCollection ? "" : "not "} a collection';
+        throw new ArgumentError('Can only map collections to collections. '
+            'Tried to map ${_m(sourceProperty)} to ${_m(targetProperty)}');
+      }
+      if (sourceProperty.isCollection) {
+        print('TODO: support mapping collections');
+      } else {}
+    }
+  }
+
+  Property _resolvePath(ValueClass fromClassifier, BuiltList<String> fromPath) {
+    final property =
+        fromClassifier.properties.firstWhere((p) => p.name == fromPath.first);
+    if (property == null || fromPath.length == 1) {
+      return property;
+    } else {
+      if (property.type is! ValueClass) {
+        throw new ArgumentError(
+            'Attempt to resolve path "${fromPath.sublist(1).join('.')}" '
+            'on non ValueClass ${property.type}');
+      }
+      return _resolvePath(property.type as ValueClass, fromPath.sublist(1));
+    }
+  }
+}
+
+class _TransformationContext {
+  ClassifierBuilder builderFor(Classifier to) {}
 }
