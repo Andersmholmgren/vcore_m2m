ea0602a88e7ce7326cf9e67ce95523a2afdf346b
Anders Holmgren <andersmholmgren@gmail.com>
2016-05-25 12:05:45 +1000

    test model

--- test/model/test_model.dart
+++ test/model/test_model.dart
@@ -1,24 +1,62 @@
 library test_model;
 
-part 'test_model.g.dart';
-
 import 'package:built_value/built_value.dart';
 import 'package:built_json/built_json.dart';
+import 'package:built_collection/built_collection.dart';
 
-abstract class Car
-  implements Built<Car, CarBuilder> {
+part 'test_model.g.dart';
+
+abstract class Car implements Built<Car, CarBuilder> {
   static final Serializer<Car> serializer = _$carSerializer;
 
+  BuiltSet<Wheel> get wheels;
 
   Car._();
 
   factory Car([updates(CarBuilder b)]) = _$Car;
 }
 
-abstract class CarBuilder
-  implements Builder<Car, CarBuilder> {
+abstract class CarBuilder implements Builder<Car, CarBuilder> {
+  SetBuilder<Wheel> wheels = new SetBuilder<Wheel>();
 
   CarBuilder._();
 
   factory CarBuilder() = _$CarBuilder;
+}
+
+abstract class Wheel
+  implements Built<Wheel, WheelBuilder> {
+  static final Serializer<Wheel> serializer = _$wheelSerializer;
+
+
+  Wheel._();
+
+  factory Wheel([updates(WheelBuilder b)]) = _$Wheel;
+}
+
+abstract class WheelBuilder
+  implements Builder<Wheel, WheelBuilder> {
+
+  WheelBuilder._();
+
+  factory WheelBuilder() = _$WheelBuilder;
+}
+
+
+abstract class Engine
+  implements Built<Engine, EngineBuilder> {
+  static final Serializer<Engine> serializer = _$engineSerializer;
+
+
+  Engine._();
+
+  factory Engine([updates(EngineBuilder b)]) = _$Engine;
+}
+
+abstract class EngineBuilder
+  implements Builder<Engine, EngineBuilder> {
+
+  EngineBuilder._();
+
+  factory EngineBuilder() = _$EngineBuilder;
 }
--- test/model/test_model.g.dart
+++ test/model/test_model.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// 2016-05-25T02:02:20.889569Z
+// 2016-05-25T02:05:39.983200Z
 
 part of test_model;
 
@@ -9,6 +9,8 @@ part of test_model;
 // **************************************************************************
 
 Serializer<Car> _$carSerializer = new _$CarSerializer();
+Serializer<Wheel> _$wheelSerializer = new _$WheelSerializer();
+Serializer<Engine> _$engineSerializer = new _$EngineSerializer();
 
 class _$CarSerializer implements StructuredSerializer<Car> {
   final Iterable<Type> types = new BuiltList<Type>([Car, _$Car]);
@@ -17,13 +19,93 @@ class _$CarSerializer implements StructuredSerializer<Car> {
   @override
   Iterable serialize(Serializers serializers, Car object,
       {FullType specifiedType: FullType.unspecified}) {
-    return [];
+    return [
+      'wheels',
+      serializers.serialize(object.wheels,
+          specifiedType:
+              const FullType(BuiltSet, const [const FullType(Wheel)])),
+    ];
   }
 
   @override
   Car deserialize(Serializers serializers, Iterable serialized,
       {FullType specifiedType: FullType.unspecified}) {
     final result = new CarBuilder();
+
+    var key;
+    var value;
+    var expectingKey = true;
+    for (final item in serialized) {
+      if (expectingKey) {
+        key = item;
+        expectingKey = false;
+      } else {
+        value = item;
+        expectingKey = true;
+
+        switch (key as String) {
+          case 'wheels':
+            result.wheels.replace(serializers.deserialize(value,
+                specifiedType:
+                    const FullType(BuiltSet, const [const FullType(Wheel)])));
+            break;
+        }
+      }
+    }
+
+    return result.build();
+  }
+}
+
+class _$WheelSerializer implements StructuredSerializer<Wheel> {
+  final Iterable<Type> types = new BuiltList<Type>([Wheel, _$Wheel]);
+  final String wireName = 'Wheel';
+
+  @override
+  Iterable serialize(Serializers serializers, Wheel object,
+      {FullType specifiedType: FullType.unspecified}) {
+    return [];
+  }
+
+  @override
+  Wheel deserialize(Serializers serializers, Iterable serialized,
+      {FullType specifiedType: FullType.unspecified}) {
+    final result = new WheelBuilder();
+
+    var key;
+    var value;
+    var expectingKey = true;
+    for (final item in serialized) {
+      if (expectingKey) {
+        key = item;
+        expectingKey = false;
+      } else {
+        value = item;
+        expectingKey = true;
+
+        switch (key as String) {
+        }
+      }
+    }
+
+    return result.build();
+  }
+}
+
+class _$EngineSerializer implements StructuredSerializer<Engine> {
+  final Iterable<Type> types = new BuiltList<Type>([Engine, _$Engine]);
+  final String wireName = 'Engine';
+
+  @override
+  Iterable serialize(Serializers serializers, Engine object,
+      {FullType specifiedType: FullType.unspecified}) {
+    return [];
+  }
+
+  @override
+  Engine deserialize(Serializers serializers, Iterable serialized,
+      {FullType specifiedType: FullType.unspecified}) {
+    final result = new EngineBuilder();
 
     var key;
     var value;
@@ -51,33 +133,118 @@ class _$CarSerializer implements StructuredSerializer<Car> {
 // **************************************************************************
 
 class _$Car extends Car {
-  _$Car._() : super._() {}
+  final BuiltSet<Wheel> wheels;
+  _$Car._({this.wheels}) : super._() {
+    if (wheels == null) throw new ArgumentError('null wheels');
+  }
   factory _$Car([updates(CarBuilder b)]) =>
       (new CarBuilder()..update(updates)).build();
   Car rebuild(updates(CarBuilder b)) => (toBuilder()..update(updates)).build();
   _$CarBuilder toBuilder() => new _$CarBuilder()..replace(this);
   bool operator ==(other) {
     if (other is! Car) return false;
-    return true;
+    return wheels == other.wheels;
   }
 
   int get hashCode {
-    return 194915866;
+    return hashObjects([wheels]);
   }
 
   String toString() {
-    return 'Car {}';
+    return 'Car {'
+        'wheels=${wheels.toString()}\n'
+        '}';
   }
 }
 
 class _$CarBuilder extends CarBuilder {
   _$CarBuilder() : super._();
-  void replace(Car other) {}
+  void replace(Car other) {
+    super.wheels = other.wheels?.toBuilder();
+  }
+
   void update(updates(CarBuilder b)) {
     if (updates != null) updates(this);
   }
 
   Car build() {
-    return new _$Car._();
+    if (wheels == null) throw new ArgumentError('null wheels');
+    return new _$Car._(wheels: wheels?.build());
+  }
+}
+
+// **************************************************************************
+// Generator: BuiltValueGenerator
+// Target: abstract class Wheel
+// **************************************************************************
+
+class _$Wheel extends Wheel {
+  _$Wheel._() : super._() {}
+  factory _$Wheel([updates(WheelBuilder b)]) =>
+      (new WheelBuilder()..update(updates)).build();
+  Wheel rebuild(updates(WheelBuilder b)) =>
+      (toBuilder()..update(updates)).build();
+  _$WheelBuilder toBuilder() => new _$WheelBuilder()..replace(this);
+  bool operator ==(other) {
+    if (other is! Wheel) return false;
+    return true;
+  }
+
+  int get hashCode {
+    return 133697385;
+  }
+
+  String toString() {
+    return 'Wheel {}';
+  }
+}
+
+class _$WheelBuilder extends WheelBuilder {
+  _$WheelBuilder() : super._();
+  void replace(Wheel other) {}
+  void update(updates(WheelBuilder b)) {
+    if (updates != null) updates(this);
+  }
+
+  Wheel build() {
+    return new _$Wheel._();
+  }
+}
+
+// **************************************************************************
+// Generator: BuiltValueGenerator
+// Target: abstract class Engine
+// **************************************************************************
+
+class _$Engine extends Engine {
+  _$Engine._() : super._() {}
+  factory _$Engine([updates(EngineBuilder b)]) =>
+      (new EngineBuilder()..update(updates)).build();
+  Engine rebuild(updates(EngineBuilder b)) =>
+      (toBuilder()..update(updates)).build();
+  _$EngineBuilder toBuilder() => new _$EngineBuilder()..replace(this);
+  bool operator ==(other) {
+    if (other is! Engine) return false;
+    return true;
+  }
+
+  int get hashCode {
+    return 1043237298;
+  }
+
+  String toString() {
+    return 'Engine {}';
+  }
+}
+
+class _$EngineBuilder extends EngineBuilder {
+  _$EngineBuilder() : super._();
+  void replace(Engine other) {}
+  void update(updates(EngineBuilder b)) {
+    if (updates != null) updates(this);
+  }
+
+  Engine build() {
+    return new _$Engine._();
   }
 }
