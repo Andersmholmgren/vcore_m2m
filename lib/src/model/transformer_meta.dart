b2c04842e9cad0069ba563ccccb27ad8b198a31e
Anders Holmgren <andersmholmgren@gmail.com>
2016-06-27 09:30:09 +1000

    transformation metamodel

--- lib/src/model/transformer_meta.dart
+++ lib/src/model/transformer_meta.dart
@@ -4,18 +4,23 @@ import 'package:built_value/built_value.dart';
 
 part 'transformer_meta.g.dart';
 
+/// meta model for a [AbstractTransformation]
 abstract class TransformationMetaModel
-  implements Built<TransformationMetaModel, TransformationMetaModelBuilder> {
+    implements Built<TransformationMetaModel, TransformationMetaModelBuilder> {
+  String get fromTypeName;
+  String get toTypeName;
 
   TransformationMetaModel._();
 
   factory TransformationMetaModel([updates(TransformationMetaModelBuilder b)]) =
-  _$TransformationMetaModel;
+      _$TransformationMetaModel;
 }
 
 abstract class TransformationMetaModelBuilder
-  implements Builder<TransformationMetaModel, TransformationMetaModelBuilder> {
-
+    implements
+        Builder<TransformationMetaModel, TransformationMetaModelBuilder> {
+  String fromTypeName;
+  String toTypeName;
   TransformationMetaModelBuilder._();
 
   factory TransformationMetaModelBuilder() = _$TransformationMetaModelBuilder;
